24bb10b432d443815f20806804906ac108670dc9
Amir Mujkic <mujkicc@gmail.com>
2021-10-01 18:57:27 +0200

    Set

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,7 +1,17 @@
 // Only change code below this line
 let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
 
-let uniqueArray = [...new Set(numberArray.sort((a, b) => a - b))];
+let uniqueArray = [];
+let mySet = new Set();
+
+for (let i = 0; i < numberArray.length; i++) {
+    mySet.add(numberArray[i]);
+}
+
+for (let num of mySet) {
+    uniqueArray.push(num);
+}
+uniqueArray.sort((a, b) => a - b)
 console.log(uniqueArray);
 
 // Only change code above this line
